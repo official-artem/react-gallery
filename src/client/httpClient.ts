85752e003e223650a8be16ab61f28c1bf0afd702
official-artem <grechuck.artem.ag@gmail.com>
2024-01-12 13:36:40 +0200

    add base to vite config

--- src/client/httpClient.ts
+++ src/client/httpClient.ts
@@ -1,6 +1,7 @@
 import { PhotoResponse } from '../types/photoResponse.type';
 
 const BASE_URL = 'https://api.unsplash.com/photos';
+const API_KEY = 'MydeBPiEISEdGAiDu4i2vsWzrpGbPfjxJpbaddrOW_Y';
 
 export async function getPhotos(): Promise<PhotoResponse[]> {
   return fetch(`${BASE_URL}?client_id=${API_KEY}`, {
@@ -13,4 +14,4 @@ export async function getPhotoById(id: string): Promise<PhotoResponse> {
   return fetch(`${BASE_URL}/${id}?client_id=${API_KEY}`)
     .then(photosFromServer => photosFromServer.json())
     .then(photosFromServer => photosFromServer);
-}``
+}
