fbb4626446933889bf3629821767ea07e8b81ce6
official-artem <grechuck.artem.ag@gmail.com>
2024-01-12 14:03:19 +0200

    change name of env key

--- src/client/httpClient.tsx
+++ src/client/httpClient.tsx
@@ -1,7 +1,7 @@
 import { PhotoResponse } from '../types/photoResponse.type';
 
 const BASE_URL = 'https://api.unsplash.com/photos';
-const API_KEY = import.meta.env.API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getPhotos(): Promise<PhotoResponse[]> {
   return fetch(`${BASE_URL}?client_id=${API_KEY}`, {
@@ -11,6 +11,7 @@ export async function getPhotos(): Promise<PhotoResponse[]> {
 }
 
 export async function getPhotoById(id: string): Promise<PhotoResponse> {
+  console.log(API_KEY)
   return fetch(`${BASE_URL}/${id}?client_id=${API_KEY}`)
     .then(photosFromServer => photosFromServer.json())
     .then(photosFromServer => photosFromServer);
