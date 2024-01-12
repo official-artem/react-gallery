ee89cc2b988638c4cebc32238cd05e911b0fde16
official-artem <grechuck.artem.ag@gmail.com>
2024-01-12 13:52:21 +0200

    change client file

--- src/client/httpClient.tsx
+++ src/client/httpClient.tsx
@@ -1,7 +1,7 @@
 import { PhotoResponse } from '../types/photoResponse.type';
 
 const BASE_URL = 'https://api.unsplash.com/photos';
-const API_KEY = process.env.API_KEY;
+const API_KEY = import.meta.env.API_KEY;
 
 export async function getPhotos(): Promise<PhotoResponse[]> {
   return fetch(`${BASE_URL}?client_id=${API_KEY}`, {
